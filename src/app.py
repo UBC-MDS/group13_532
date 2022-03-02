4f4ddab3e6a5fbe2feb3b97f352fa58a311a4ff4
mahsasarafrazi <Ma.sarafrazi@gmail.com>
2022-03-01 23:28:09 -0800

    dash partitions

--- src/app.py
+++ src/app.py
@@ -5,32 +5,60 @@ import pandas as pd
 
 
 data = pd.read_csv('data/processed/clean_df.csv')
-plot=alt.Chart(data).mark_line().encode(
-         x='release_year',
-         y='count()',
-         color='rating')
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 server= app.server
 
 app.layout = dbc.Container([
-    html.H1('Netflix Movie Trend'),
+    html.Br(),
+    dbc.Row(dbc.Col(html.Div("Netflix Movie Trend"),
+                width={"size": 3, "offset": 5},
+                style={'font-weight': 'bold'})),
     dbc.Row(
-        html.Iframe(
+            [
+                dbc.Col(dbc.Card(dbc.CardBody(html.H5('Sufang Part'))),
+                       ),
+            ]),
+    html.Br(),
+    dbc.Row([
+                dbc.Col(dbc.Card(dbc.CardBody(html.H5('Jasmin Part'))),
+                       width= 5),
+                dbc.Col([
+                    dbc.Label("Year", html_for="range-slider"),
+                    html.Div([
+                    dcc.RangeSlider(id="range-slider", min = 1970, max = 2020, value=[1995, 2020],  marks={
+                        1970: "1970",
+                        1975: "1975",
+                        1980: "1980",
+                        1985: "1985",
+                        1990: "1990",
+                        1995: "1995",
+                        2000: "2000",
+                        2005: "2005",
+                        2010: "2010",
+                        2015: "2015",
+                        2020: "2020",
+            }),
+         html.Iframe(
                 id='line',
-                srcDoc=plot.to_html(),
-                style={'border-width': '0', 'width': '100%', 'height': '400px'})
-    )
+                style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+    ])
+                ]),
+            ]
+        ),
     ])
 
 
+@app.callback(
+    Output('line', 'srcDoc'),
+    Input('range-slider', 'value'))
 
-# def plot_altair():
-#     plot=alt.Chart(data).mark_line().encode(
-#         x='release_year',
-#         y='count()',
-#         color='rating')
-#     return plot.to_html()
+def rating_plot(year_range):
+    line_plot = alt.Chart(data[(data['release_year']> year_range[0]) & (data['release_year']< year_range[1])]).mark_line().encode(
+                x='release_year:O',
+                y='count():Q',
+                color='rating:O').interactive()
+    return line_plot.to_html()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
