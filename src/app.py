b9e74059819719e7ab7e72793649f76fd9212e16
Jasmine Ortega <jorteg01@ubc.ca>
2022-03-03 10:44:34 -0800

    Add in cast count plot

--- src/app.py
+++ src/app.py
@@ -3,9 +3,18 @@ import altair as alt
 import dash_bootstrap_components as dbc
 import pandas as pd
 
+alt.data_transformers.disable_max_rows()
 
 data = pd.read_csv("data/processed/clean_df.csv")
 
+# Jasmine data wrangling
+
+
+df["cast_list"] = df["cast"].str.split(",")
+df["cast_count"] = df["cast_list"].str.len()
+cast_df = df[["title", "cast", "listed_in", "cast_count", "release_year"]]
+
+# Mahsa data wrangling
 rating_list = [
     "TV-G",
     "TV-14",
@@ -49,8 +58,32 @@ app.layout = dbc.Container(
         html.Br(),
         dbc.Row(
             [
-                ############# Jasmin Part
-                dbc.Col(dbc.Card(dbc.CardBody(html.H5("Jasmin Part"))), width=5),
+                # Jasmine Part
+                dbc.Col(
+                 html.Div(
+                     [
+                    html.Iframe(
+                        id="scatter", 
+                        srcDoc=plot_cast(xmax=2020),
+                       style={
+                           'border-width': '0', 
+                           'width': '100%', 
+                           'height': '400px'
+                       }
+                    ),
+                    dcc.Slider(id='xslider', 
+                               min=1942, max=2020,
+                                marks={
+                                    1942: '1942', 
+                                       1962: '1962',
+                                       1980:'1980',
+                                       2000: '2000',
+                                       2020:'2020'
+                                }
+                              )
+                     ]
+                 )
+                ),
                 # Mahsa Part
                 dbc.Col(
                     [
@@ -108,9 +141,31 @@ app.layout = dbc.Container(
 
 @app.callback(
     Output("line", "srcDoc"),
+    Output('scatter', 'srcDoc'),
     Input("range-slider", "value"),
     Input("rating_widget", "value"),
+    Input('xslider', 'value'),
 )
+
+# Jasmine plot function
+def plot_cast(xmax):
+    cast_plot = alt.Chart(cast_df[cast_df["release_year"] < xmax], title = "Average Cast Size Per Year").mark_circle().encode(
+        x = alt.X("release_year",  
+                  title = "Movie Release Year", 
+                  scale=alt.Scale(domain=[1935, 2022]), 
+                 axis=alt.Axis(format='f')),
+        
+        y = alt.Y("mean(cast_count)", 
+                  title="Average Cast Size",
+                  axis=alt.Axis(tickMinStep=1)
+                 )
+    )
+    return cast_plot.to_html()
+
+def update_output(xmax):
+    return plot_cast(xmax)
+
+# Mahsa plot function
 def rating_plot(year_range, ratings):
     line_plot = (
         alt.Chart(
